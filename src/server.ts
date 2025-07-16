4fee8b6d641d58ea74c04583f0c70a2ea01e3ef9
Neto Rodrigues <accounts@rodrigsneto.com>
2025-07-16 18:40:01 -0300

    chore(db): installing prisma

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,20 @@
 import express from "express";
 import cors from 'cors';
 import dotenv from 'dotenv';
+import router from "./routes";
 
+dotenv.config();
+const PORT = process.env.PORT;
+
+const server = express();
+
+server.use(cors());
+server.use(express.json());
+
+server.use('/', router)
+
+export default server;
+
+server.listen(PORT, () => {
+  console.log(`🚀 Servidor rodando na porta ${PORT}`);
+});
